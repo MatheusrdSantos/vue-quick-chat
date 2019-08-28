20dc00f0fff26f4adf36d1a413b722323c6e60cb
Matheus Ricardo dos Santos <matheus.r.d.s10@hotmail.com>
2019-08-28 20:09:03 -0300

    Fix lib build for dist

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import Vue from 'vue'
-import App from './App.vue'
 import feather from 'vue-icon'
 import store from './store'
 Vue.config.productionTip = false
 Vue.use(feather, 'v-icon')
-
-new Vue({
-  store,
-  render: h => h(App)
-}).$mount('#app')
-
 export { default as Chat } from './components/Chat.vue';
