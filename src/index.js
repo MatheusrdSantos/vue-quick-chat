4bc804aa763c539be62a10c8f26e942171bbb1e2
Tofandel <adrien.foulon@tukan.hu>
2019-10-08 04:30:22 +0200

    Missed the other icons so leave vue-icon for now

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import feather from 'vue-icon'
 import store from './store'
 Vue.config.productionTip = false
+Vue.use(feather, 'v-icon')
 export { default as Chat } from './components/Chat.vue';
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import feather from 'vue-icon'
 import store from './store'
 Vue.config.productionTip = false
+Vue.use(feather, 'v-icon')
 
 new Vue({
   store,
