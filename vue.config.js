ba5668d10d4709513f0ffe9fcbdcbed497d3e676
Tofandel <adrien.foulon@tukan.hu>
2019-11-08 12:07:27 +0100

    Compile without whitespaces in templates

--- vue.config.js
+++ vue.config.js
@@ -18,5 +18,15 @@ module.exports = {
                 })
             ]
         }
+    },
+    chainWebpack(config) {
+        config.module
+            .rule("vue")
+            .use("vue-loader")
+            .loader("vue-loader")
+            .tap(options => {
+                options.compilerOptions.whitespace = "condense";
+                return options
+            });
     }
 };
